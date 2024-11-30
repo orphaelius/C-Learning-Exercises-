cb6e9234dcfc6fa3e6c0d0e5ec4af2ee916bee14
Jon-Austin <75102416+orphaelius@users.noreply.github.com>
2024-11-30 12:28:23 -0500

    Update 4.15CompoundInterest.cpp

--- 4.15CompoundInterest.cpp
+++ 4.15CompoundInterest.cpp
@@ -7,7 +7,6 @@
 * its steps for interest rates of 5%, 6%, 7%, 8%, 9%, and 10%.
 * use a for loop to vary the interest rate */
 
-
 int main(void) {
 
 	double principal = 1000.0;
@@ -20,13 +19,9 @@ int main(void) {
 
 		for (int year = 1; year <= 10; ++year) {
 			double amount = principal * pow(1.0 + rate, year);
-			printf("%4d%21.2f\n", year, amount);
-			
-		}		
-		
-		puts("");		
-		
+			printf("%4d%21.2f\n", year, amount);			
+		}				
+		puts("");			
 	}
-
 }
 
