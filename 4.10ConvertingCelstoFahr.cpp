a347077209a54a7d0847c8353c1cf2f889d993a3
Jon-Austin <75102416+orphaelius@users.noreply.github.com>
2024-11-30 12:26:37 -0500

    Update 4.10ConvertingCelstoFahr.cpp

--- 4.10ConvertingCelstoFahr.cpp
+++ 4.10ConvertingCelstoFahr.cpp
@@ -11,33 +11,15 @@
 * F = 9/5(C) + 32 //
 */
 
-/*
-* initialize function:
-* 
-* intialize variables:
-* celsius = 30;
-* fahrenheit = 0;
-* * 
-* program runs from 30c to 50c:
-* while celsius <= 50
-* convert current cels value to fahrenheit
-* print celsius and fahrenheit
-* celsius ++;
-*/
-
 int main(void) {
 	int celsius = 30; 
 	double fahr = 0;
-
+	
 	printf("Celsius Temperature %15s Fahrenheit Conversion\n\n", "");
 
-
 	for (celsius; celsius <= 50; celsius++) {  
-
 		fahr = (celsius * (9.0 / 5.0) + 32);
-
 		printf("%16s %d %32s %.1f\n", "", celsius, "", fahr);  
 	}
-
 }
 
