98d926072dc325b692f0cd3174d86152fa4bdb9b
Jon-Austin <75102416+orphaelius@users.noreply.github.com>
2025-05-24 12:00:58 -0400

    Update cipherHeader.h

--- Ch 8/Challenges/VigenereCipher/cipherHeader.h
+++ Ch 8/Challenges/VigenereCipher/cipherHeader.h
@@ -1,2 +1,8 @@
-void encrypt(char stringToEncrypt[], char encryptedString[], char secretKeyString[]); 
-void decrypt(char stringToDecrypt[], char decryptedString[], char secretKeyString[]);     
+
+void checkKey(char secretKey[], int* isFalse); 
+void getString(char string[]);
+void encrypt(char* secretKey, char* stringToEncrypt, char* encryptedString);
+void decrypt(char* secretKey, char* decryptedString, char* stringToDecrypt);
+int getKey(int* switchKey);
+void showInstructions(void);
+void rinsePalate();
